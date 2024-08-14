c2f09e37eaa9aee84312eef2a37c1914986aa4e3
WAAZKOR <106280843+WAAZKOR@users.noreply.github.com>
2024-08-14 15:05:39 -0700

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,4 @@
-var map = L.map("map").setView([32.2540, -110.9742], 13);
+var map = L.map('map').setView([32.2540, -110.9742], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
