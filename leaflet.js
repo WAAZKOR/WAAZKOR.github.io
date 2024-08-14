8c4066ab20e7ca498fd7dff12335bbd574f2e21a
WAAZKOR <106280843+WAAZKOR@users.noreply.github.com>
2024-08-14 10:22:41 -0700

    Update leaflet.js - troubleshoot map drawing

--- leaflet.js
+++ leaflet.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([51.505, -0.09], 13); 
+var map = L.map('map').setView([32.253460, -110.911789], 13); 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>   
- contributors'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 function   
